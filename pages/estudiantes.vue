d39a095ec9bfd4bd8a001f4b53f69ff74c83fa80
Eloy Ramirez <clustersnake45@gmail.com>
2024-05-07 16:03:27 -0400

    feat: Crear página de profesores y estudiantes

---
+++ pages/estudiantes.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// code...
+</script>
+
+<template>
+  <!-- code... -->
+  <h1 class="text-3xl">
+    estudiantes
+  </h1>
+  
+</template>
+
+<style scoped>
+/* code... */
+</style>
---
+++ pages/profesores.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// code...
+</script>
+
+<template>
+  <h1 class="text-3xl">
+    profesores
+  </h1>
+  
+  <!-- code... -->
+</template>
+
+<style scoped>
+/* code... */
+</style>
