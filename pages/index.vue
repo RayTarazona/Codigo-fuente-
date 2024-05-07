09af71fa253ea4c693b641382d72729a1767d61e
Eloy Ramirez <clustersnake45@gmail.com>
2024-05-07 09:17:11 -0400

    feat: Crear página de ejemplo

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// code...
+  const onclick = () => {
+    alert('clicked')
+  }
+</script>
+
+<template>
+  <!-- code... -->
+  <div class="">
+    <h1 class="text-3xl">Index page!</h1>
+    <button @click="onclick" class="btn btn-primary">click</button>
+  </div>
+</template>
+
+<style scoped>
+/* code... */
+</style>
