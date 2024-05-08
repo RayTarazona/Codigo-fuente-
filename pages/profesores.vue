d26046d1d0f8d0b6fc64a501df75af25c088d016
Eloy Ramirez <clustersnake45@gmail.com>
2024-05-07 22:58:34 -0400

    fix: Envolver contentido de las paginas en section

--- pages/profesores.vue
+++ pages/profesores.vue
@@ -3,19 +3,21 @@
 </script>
 
 <template>
-  <h1 class="text-3xl">
-    profesores 
-  </h1>
-  <h1>
-    <i class="fas fa-person-chalkboard fa-2x"></i>
-  </h1>
-  <h1>
-    <i class="fas fa-chalkboard-user fa-2x"></i>
-  </h1>
-  <h1>
-    <i class="fas fa-person-burst fa-2x fa-flip-horizontal"></i>
-    <i class="fas fa-person-falling fa-2x fa-flip-horizontal"></i>
-  </h1>
+  <section>
+    <h1 class="text-3xl">
+      profesores 
+    </h1>
+    <h1>
+      <i class="fas fa-person-chalkboard fa-2x"></i>
+    </h1>
+    <h1>
+      <i class="fas fa-chalkboard-user fa-2x"></i>
+    </h1>
+    <h1>
+      <i class="fas fa-person-burst fa-2x fa-flip-horizontal"></i>
+      <i class="fas fa-person-falling fa-2x fa-flip-horizontal"></i>
+    </h1>
+  </section>
   
   <!-- code... -->
 </template>
