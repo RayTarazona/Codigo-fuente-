8cfd1645d2442337d061f0e73cc83823937fa7c9
Eloy Ramirez <clustersnake45@gmail.com>
2024-05-07 22:44:12 -0400

    fix: Ajustar ruta de páginas en el enlace

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,10 +13,10 @@
             <nuxt-link to="/"class="text-base-100" active-class="active"><i class="fas fa-home fa-xl"></i> Inicio</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="profesores" class="text-base-100" active-class="active"><i class="fas fa-person-chalkboard fa-xl"></i> Profesores</nuxt-link>
+            <nuxt-link to="/profesores" class="text-base-100" active-class="active"><i class="fas fa-person-chalkboard fa-xl"></i> Profesores</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="estudiantes" class="text-base-100" active-class="active"><i class="fas fa-person-rifle text-lg"></i> Estudiantes</nuxt-link>
+            <nuxt-link to="/estudiantes" class="text-base-100" active-class="active"><i class="fas fa-person-rifle text-lg"></i> Estudiantes</nuxt-link>
           </li>
 
         </menu>
