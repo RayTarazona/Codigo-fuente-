1e04c8d6c25009c5c1144af8640228387b141d0e
Eloy Ramirez <clustersnake45@gmail.com>
2024-05-14 18:49:01 -0400

    feat: Crear component para navegacion lateral
    - Crear menu desplegable para profesores y estudiantes

---
+++ components/AppSideNav.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+  const drawerShown = useState('drawerShown', () => true);
+</script>
+
+<template>
+  <nav class="pt-4 text-base-100">
+    <menu class="menu">
+      <li>
+        <nuxt-link to="/"class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-border-all fa-xl"></i></button>
+          <span v-show="drawerShown" >
+            Inicio
+          </span>
+        </nuxt-link>
+      </li>
+      <li>
+        <details >
+        <summary>
+            <button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-person-chalkboard fa-xl"></i></button>
+            <span v-show="drawerShown" >
+              Profesores
+            </span>
+        </summary>
+        <ul>
+          <li>
+          <nuxt-link to="/profesores" class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-list fa-xl"></i></button>
+            <span v-show="drawerShown" >
+              Listar
+            </span>
+          </nuxt-link>
+          </li>
+          <li>
+          <nuxt-link to="/profesores/agregar" class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-circle-plus fa-xl"></i></button>
+            <span v-show="drawerShown" >
+              Agregar
+            </span>
+          </nuxt-link>
+          </li>
+        </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary>
+              <button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-users fa-xl"></i></button>
+              <span v-show="drawerShown" >
+                Estudiantes
+              </span>
+          </summary>
+          <ul>
+            <li>
+            <nuxt-link to="/estudiantes" class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-list fa-xl"></i></button>
+              <span v-show="drawerShown" >
+                Listar
+              </span>
+            </nuxt-link>
+            </li>
+            <li>
+            <nuxt-link to="" class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-circle-plus fa-xl"></i></button>
+              <span v-show="drawerShown" >
+                Agregar
+              </span>
+            </nuxt-link>
+            </li>
+          </ul>
+        </details>
+      </li>
+
+    </menu>
+  </nav>
+</template>
+
+<style scoped>
+/* code... */
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,12 @@
 <script setup lang="ts">
 // code...
-  const drawerShown = useState('drawerShown');
 </script>
 
 <template class="">
   <section class="min-h-screen h-full flex flex-col bg-primary">
     <app-header />
       <main class="flex-1 flex">
-        <nav class="pt-4">
-          <menu class="menu">
-            <li>
-              <nuxt-link to="/"class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-border-all fa-xl"></i></button>
-                <span v-show="drawerShown" >
-                  Inicio
-                </span>
-              </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link to="/profesores" class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-person-chalkboard fa-xl"></i></button>
-                <span v-show="drawerShown" >
-                  Profesores
-                </span>
-              </nuxt-link>
-            </li>
-            <li>
-              <nuxt-link to="/estudiantes" class="text-base-100" active-class="active"><button class="btn btn-ghost btn-square btn-xs"><i class="fas fa-users fa-xl"></i></button>
-                <span v-show="drawerShown" >
-                  Estudiantes
-                </span>
-              </nuxt-link>
-            </li>
-
-          </menu>
-        </nav>
+        <app-side-nav />
         <section class="flex-1 bg-base-100 pt-4 px-2">
           <slot />
         </section>
