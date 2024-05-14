7ee34dc2f6f1d21646a5f7b687eb6f2d6a21204e
Eloy Ramirez <clustersnake45@gmail.com>
2024-05-14 18:50:13 -0400

    feat: Crear pagina para registrar profesores

---
+++ pages/profesores/agregar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth'
+})
+// code...
+</script>
+
+<template>
+  <section>
+    <h1 class="text-3xl">
+      Registrar
+    </h1>
+  </section>
+</template>
+
+<style scoped>
+/* code... */
+</style>
